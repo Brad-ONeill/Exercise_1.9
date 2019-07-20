a856b4f5275b99fab82cbd90b9fc0df883c833e1
Bradley O'Neill <bradley.oneill@gmail.com>
2019-07-20 17:51:45 +0100

    Removed some comments and empty space

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var pokemonRepository = (function () {								//pokemonRepository is the name of the IIFE
-	var repo = [];													//repo is the name of the array
-	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';	//specifying maximum limit to call from the API
+var pokemonRepository = (function () { //pokemonRepository is the name of the IIFE
+	var repo = []; //repo is the name of the array
+	var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151'; //specifying maximum limit to call from the API
 
 	function add(pokemon) {
 		repo.push(pokemon);
@@ -10,22 +10,28 @@ var pokemonRepository = (function () {								//pokemonRepository is the name of
 		return repo;
 	}
 
+
+	//-----
 	function loadList() {
 		return fetch(apiUrl).then(function (response) {
 			return response.json();
 		}).then(function (json) {
 			json.results.forEach(function (item) {
+
 				var pokemon = {
 					name: item.name,
 					detailsUrl: item.url
 				};
+
 				add(pokemon);
 			});
+
 		}).catch(function (e) {
 			console.error(e);
 		});
 	}
 
+	//-----
 	function loadDetails(item) {
 		var url = item.detailsUrl;
 
@@ -34,52 +40,52 @@ var pokemonRepository = (function () {								//pokemonRepository is the name of
 		}).then(function (details) {
 			item.imageUrl = details.sprites.front_default;
 			item.height = details.height;
-			var types = [];											//element types are stored as arrays so...
-			
-			details.types.forEach(function(item) {					//for each pokemon...
-				types.push(item.type.name)							//push the array details into var types...
+			var types = []; //element types are stored as arrays so...
+
+			details.types.forEach(function (item) { //for each pokemon...
+				types.push(item.type.name) //push the array details into var types...
 			});
-			
-			item.types = types										//then identify the array as [object].type to be called on line 52
-			
+
+			item.types = types //then identify the array as [object].type to be called on line 52
+
 		}).catch(function (e) {
 			console.error(e);
 		});
 	}
-	
-	function showDetails(pokemon) { 
+
+	//-----
+	function showDetails(pokemon) {
 		//loads the details (line 29) of the pokemon that was clicked THEN...
-		pokemonRepository.loadDetails(pokemon).then(function(){ 							
+		pokemonRepository.loadDetails(pokemon).then(function () {
 			//calls [displays] the modal and populates it with the name, image, height and types
 			showModal(pokemon.name, pokemon.imageUrl, pokemon.height, pokemon.types)
 		})
 	}
-	
-	function addListItem(entry) {									//this is the function used to create the list
-		
-		//creates a li HTML element
-		var $li = document.createElement('li');
-		$li.classList.add('pokedexItem');							//assigns class pokdesItem to the li element
-	
-		//creates a ul HTML element
-		var $ul = document.querySelector('ul');
-		$ul.appendChild($li);										//attaches ul element to li element created above
-		
-		//creates a button HTML element
-		var $info_button = document.createElement('button');
-		$info_button.classList.add('infoButton');					//assigns the class "infoButton" to the element
-		$info_button.setAttribute('id', "modal-button");			//assigns the id "modal-button" to the element
-		$info_button.innerHTML = entry.name;						//populate the button with the name of the pokemon fro the array
-		$li.appendChild($info_button);								//attaches the button to the li HTML element
 
-		//event listener
-		$info_button.addEventListener('click', function (event) { 	//if the user clicks the button...
-			showDetails(entry);										//show the details of that entry [pokemon]
+	//-----
+	function addListItem(entry) {
+
+		var $ul = $('.pokeList');
+
+		var $li = ('<li class="pokedexItem"></li>');
+		$('ul').append($li);
+
+		//button
+		var $info_button = $('<button class="infoButton">')
+			.attr('id', "modal-button")
+			.html(entry.name);
+
+		$('li').each(function () {
+			$(this).append($info_button);
+
+			$('.infoButton')
+				.on('click', function () {
+					showDetails(entry).each;
+				});
 		});
 
 	}
 
-	//function returns
 	return {
 		add: add,
 		getALL: getALL,
@@ -90,10 +96,9 @@ var pokemonRepository = (function () {								//pokemonRepository is the name of
 
 })();
 
-pokemonRepository.loadList().then(function () {						//loads the full list of pokemon from the API
-	// Now the data is loaded!
-	pmon.forEach(function (entry) {									//for each entry in the array...
-		pokemonRepository.addListItem(entry);						//create a list item (line 58) until specified limit (line 3) is reached
+pokemonRepository.loadList().then(function () {
+	pmon.forEach(function (entry) {
+		pokemonRepository.addListItem(entry);
 	});
 });
 
